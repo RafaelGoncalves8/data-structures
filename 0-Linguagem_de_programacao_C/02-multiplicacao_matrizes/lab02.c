dff93fc9157881305ad912f396c6ae9b9dd90ea5
Rafael Goncalves <rafaelgoncalves258@gmail.com>
2018-08-12 22:40:16 -0300

    Removed blank space at end of matrix A*B and corrected the comparison of permutation vector

--- 0-Linguagem_de_programacao_C/02-multiplicacao_matrizes/lab02.c
+++ 0-Linguagem_de_programacao_C/02-multiplicacao_matrizes/lab02.c
@@ -3,7 +3,8 @@
  *
  * O objetivo do programa é imprimir os elementos de A*B onde A é a
  * matriz de permutacao criada a partir do vetor dado p e B é a
- * matriz dada como entrada.*/
+ * matriz dada como entrada, ambas de dimensao n.*/
+
 
 #include <stdio.h>
 
@@ -14,14 +15,17 @@
 
 void create_permutation_matrix(int p[], int A[][N], int n) {
     /* cria a matriz de permutacao A a partir do vetor p */
+
     int i, j;
 
-    for (i = 0; i < n; i++)
-        for (j = 0; j < n; j++)
-            if (p[j] == i)
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            if (p[j] == i+1)
                 A[i][j] = 1;
             else
                 A[i][j] = 0;
+        }
+    }
 }
 
 
@@ -37,20 +41,21 @@ void print_matrix_multiplication (int A[][N], double B[][N],
             c = 0;
             for (k = 0; k < n; k++)
                 c += A[i][k]  * B[k][j];
-            printf("%.1f ", c);
+            if (j == n-1)
+                printf("%.1f\n", c);
+            else
+                printf("%.1f ", c);
         }
-        printf("\n");
     }
 }
 
 
 int main(){
     int i, j;
-    int n;          /* dimensao das matrizes */
+    int n;             /* dimensao das matrizes */
     int p[N];          /* vetor de permutacao */
-    int A[N][N];
-    double B[N][N];
-
+    int A[N][N];       /* matriz de permutacao */
+    double B[N][N];    /* matriz B que sera lida */
 
     /* leitura de variáveis */
     scanf("%d", &n);
@@ -62,14 +67,9 @@ int main(){
         for (j = 0; j < n; j++)
             scanf("%lf", &B[i][j]);
 
-
-    /* criacao da matriz de permutacao */
     create_permutation_matrix(p, A, n);
 
-
-    /* multiplicacao de A e B */
     print_matrix_multiplication(A, B, n);
-
 
     return 0;
 }
