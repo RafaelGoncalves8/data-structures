999daf417fa275b885fbaec5136cbb6c9c46c405
Rafael Goncalves <rafaelgoncalves258@gmail.com>
2018-09-01 23:45:55 -0300

    created sort function

--- labs/05-sorteio/lab05.c
+++ labs/05-sorteio/lab05.c
@@ -11,6 +11,8 @@
 
 #define MAX_STR 30
 #define INITIAL_SPACES 4
+#define NUM_ERR_OUT_OF_MEM -1
+#define STR_ERR_OUT_OF_MEM "Nao ha espaco na memoria."
 
 /* Registro com 2 campos: nome e numero de telefone. */
 typedef struct Entry_Struct
@@ -42,16 +44,24 @@ copy_entries(Entry * a, Entry * b, int n)
  * Entradas: registro antigo, tamanho atual n.
  *
  * Saida: novo registro com o dobro de tamanho (2n).
- *
- * TODO: check if new_entries = NULL and send error code.
  */
 Entry *
 double_entries(Entry * entries, int n)
 {
   Entry * new_entries;
 
+  /*
+   * Aloca a memoria para o vetor e checa se teve espaco,
+   * senao imprime erro.
+   */
   new_entries = malloc(2*n * sizeof(Entry));
+  if (new_entries == NULL)
+  {
+    printf(STR_ERR_OUT_OF_MEM);
+    exit(NUM_ERR_OUT_OF_MEM);
+  }
 
+  /* Copia as entradas para o novo vetor. */
   copy_entries(entries, new_entries, n);
 
   free(entries);
@@ -59,7 +69,48 @@ double_entries(Entry * entries, int n)
   return new_entries;
 }
 
-void print_winner(Entry * entries, int k) {}
+/*
+ *
+ */
+void bubble_sort(Entry * entries, int n)
+{
+  /*
+   * Variaveis para alocacao temporaria de valores para a troca de posicao dos
+   * elementos no algoritimo de ordenacao.
+   */
+  int tmp_num;
+  char tmp_name[MAX_STR];
+
+  int i;       /* Indexador para o laco for. */
+  int swapped; /* Flag que indica se houve troca ou nao.*/
+
+  do
+  {
+    swapped = 0;
+    for (i= 0; i < n - 2; i++)
+    {
+      if (entries[i].num < entries[i+1].num)
+      {
+        /* Troca entre os elemento i e i+1. */
+        tmp_num = entries[i].num;
+        strcpy(tmp_name, entries[i].name);
+        entries[i].num = entries[i+1].num;
+        strcpy(entries[i].name, entries[i+1].name);
+        entries[i+1].num = tmp_num;
+        strcpy(entries[i+1].name, tmp_name);
+
+        swapped = 1;
+      }
+    }
+  }
+  while (swapped);
+}
+
+
+
+void print_winner(Entry * entries, int k)
+{
+}
 
 int
 main()
@@ -75,8 +126,8 @@ main()
   entries = (Entry *) malloc(2 * sizeof(Entry));
   if (entries == NULL)
   {
-    printf("Não há espaço na memória.");
-    return 1;
+    printf(STR_ERR_OUT_OF_MEM);
+    return NUM_ERR_OUT_OF_MEM;
   }
 
   spaces_left = INITIAL_SPACES;
@@ -95,6 +146,9 @@ main()
   }
 
   scanf("%d", &k);
+
+  /* Ordenar vetor de structs. */
+  bubble_sort(entries, n);
 
   /* Imprime o vencedor. */
   print_winner(entries, k);
