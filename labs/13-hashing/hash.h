1ebed193171d6a4f21b833a1d0654b4d10463e64
Rafael Goncalves <rafaelgoncalves258@gmail.com>
2018-11-02 13:07:23 -0300

    Can read names and merge them in alphabetical order.

--- labs/13-hashing/hash.h
+++ labs/13-hashing/hash.h
@@ -2,13 +2,16 @@
 #define hash_h
 
 /* 
- * For each name 1 char for first letter, 15 char for last name and 1 char for '\0'. */
-#define MAX_CHAR 34
+ * For each name 1 char for first letter, 15 char for last name and 1 char
+ * for '\0'.
+ */
+#define MAX_CHAR_NAME 17
+#define MAX_CHAR_ENTRY 33
 #define MAX_ENTRIES 2048
 
 typedef struct hash
 {
-    char v[MAX_ENTRIES][MAX_CHAR];  /* Tabela de valores da hash. */
+    char v[MAX_ENTRIES][MAX_CHAR_ENTRY];  /* Tabela de valores da hash. */
 } Hash;
 
 typedef Hash * p_hash;
--- labs/13-hashing/lab13.c
+++ labs/13-hashing/lab13.c
@@ -6,6 +6,8 @@
  */
 
 #include "hash.h"
+#include <stdio.h>
+#include <string.h>
 
 int
 read_name(char * name)
@@ -19,46 +21,72 @@ read_name(char * name)
   return ans;
 }
 
-char *
-merge_names(char * s1, char * s2)
+void
+merge_names(char * s1, char * s2, char * ans)
 {
+  int i = -1, j = 0;
+
+  if (strncmp(s1, s2, MAX_CHAR_NAME) > 0)
+  {
+    while (s2[++i] != '\0')
+      ans[i] = s2[i];
+    while (s1[j] != '\0')
+      ans[i++] = s1[j++];
+  }
+  else
+  {
+    while (s1[++i] != '\0')
+      ans[i] = s1[i];
+    while (s2[j] != '\0')
+      ans[i++] = s2[j++];
+  }
+
+  ans[i] = '\0';
 }
 
 
 int
 main()
 {
+  int i;
   int m, n;
-  int last;
+  int is_first, is_last;
+  /* p_hash h; */
+  char first[MAX_CHAR_NAME], second[MAX_CHAR_NAME];
+  char entry[MAX_CHAR_ENTRY];
 
   scanf("%d %d", &n, &m);
 
-  h = create_hash_table();
+  /* h = create_hash_table(); */
 
   for (i = 0; i < m; i++)
   {
-    last = 1;
+    is_first = 1;
     do
     {
-      if (last) /* So sera 1 no primeiro nome de cada linha. */
+      if (is_first) /* Sera 1 no primeiro nome de cada linha. */
       {
-        last = read_name(first);
+        is_first = 0;
+        is_last = read_name(first);
+        printf("\nfirst_name: %s\n", first);
       }
-      else
+      else /* Nao eh o primeiro nome. */
       {
-        last = read_name(second);
-        entry = merge_names(first, second);
-        add_entry(h, entry);
+        is_last = read_name(second);
+        merge_names(first, second, entry);
+        printf("%s\n", entry);
+        /* add_entry(h, entry); */
+        /* printf("%s\n", h->v[entry]); */
       }
     }
-    while (!last);
+    while (!is_last); /* Continue enquanto nao for o ultimo nome. */
   }
 
-  for (i = 0; i < n, i++)
-  {
-  }
+  /* for (i = 0; i < n, i++) */
+  /* { */
+  /* } */
 
-  destroy_hash_table();
+  /* destroy_hash_table(); */
 
   return 0;
 
